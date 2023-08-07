af4343959816fc9ddcacf16dfcafa649598f2abd
someaddons <someaddons@github.com>
2023-08-07 13:36:34 +0200

    1.8: Add safeguards for performance mods breaking vanilla logic

---
+++ src/main/java/com/betterchunkloading/chunk/IDistanceManagerCleanup.java
@@ -0,0 +1,8 @@
+package com.betterchunkloading.chunk;
+
+import com.betterchunkloading.chunk.PlayerChunkData;
+
+public interface IDistanceManagerCleanup
+{
+    void betterchunkloading$cleanPlayer(PlayerChunkData player);
+}

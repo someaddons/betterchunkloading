f8689684eae9f8066a11e146b520cd911acf74f6
someaddons <someaddons@github.com>
2023-08-10 12:53:17 +0200

    1.18.2

--- src/main/java/com/betterchunkloading/mixin/PlayerChunkDataMixin.java
+++ src/main/java/com/betterchunkloading/mixin/PlayerChunkDataMixin.java
@@ -6,27 +6,19 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.player.ProfilePublicKey;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(ServerPlayer.class)
-public abstract class PlayerChunkDataMixin extends Player implements IPlayerDataPlayer
-{
-    public PlayerChunkDataMixin(
-            Level level,
-            BlockPos blockPos,
-            float f,
-            GameProfile gameProfile,
-            @Nullable ProfilePublicKey profilePublicKey) {
-        super(level, blockPos, f, gameProfile, profilePublicKey);
+public abstract class PlayerChunkDataMixin extends Player implements IPlayerDataPlayer {
+
+    public PlayerChunkDataMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile) {
+        super(level, blockPos, f, gameProfile);
     }
 
     @Override
-    public PlayerChunkData betterchunkloading$getPlayerChunkData()
-    {
+    public PlayerChunkData betterchunkloading$getPlayerChunkData() {
         return playerChunkData;
     }
 

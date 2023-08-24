7fcc9fbcf889b6bddba5573d19cd1efe5b81198b
someaddons <someaddons@github.com>
2023-08-24 22:25:18 +0200

    2.1: Fix an issue where sometimes some chunks stayed loaded too long Add command to display existing player tickets

--- src/main/java/com/betterchunkloading/chunk/IDistanceManagerCleanup.java
+++
@@ -1,8 +0,0 @@
-package com.betterchunkloading.chunk;
-
-import com.betterchunkloading.chunk.PlayerChunkData;
-
-public interface IDistanceManagerCleanup
-{
-    void betterchunkloading$cleanPlayer(PlayerChunkData player);
-}

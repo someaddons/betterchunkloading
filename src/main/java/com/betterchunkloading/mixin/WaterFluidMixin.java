f180f29ce3ee64d982130956d69a2c8d75acc9d8
someaddons <someaddons@github.com>
2025-01-27 15:12:45 +0100

    5.3: Improve fluid spreading performance

---
+++ src/main/java/com/betterchunkloading/mixin/WaterFluidMixin.java
@@ -0,0 +1,17 @@
+package com.betterchunkloading.mixin;
+
+import com.betterchunkloading.config.CommonConfiguration;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.WaterFluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = WaterFluid.class, priority = 5000)
+public class WaterFluidMixin
+{
+    @Overwrite
+    protected boolean canConvertToSource(Level p_256670_)
+    {
+        return CommonConfiguration.convertWaterSource;
+    }
+}
